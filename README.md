695b4c53083c509e0db700edecb4cd1edb506251
Nicolas Ledez <github.public@ledez.net>
2020-01-22 23:33:58 +0100

    Add vault kv management

---
+++ README.md
@@ -0,0 +1,13 @@
+# Install
+
+```
+./install.sh
+```
+
+# Launch
+
+```
+export VAULT_TOKEN=x.XXX
+python push_kv.py --filename dev.yaml --filename dev-password.yml
+python push_kv.py --filename dev.yaml --filename dev-password.yml --update
+```
--- push_kv.py
+++ push_kv.py
@@ -2,11 +2,15 @@
 
 import argparse
 import consul
+import hvac
 import sys
 import yaml
 
+VAULT_KV_MOUNT = 'kv'
+
 update = False
-need_to_create_or_update = {}
+consul_to_create_or_update = {}
+vault_to_create_or_update = {}
 
 parser = argparse.ArgumentParser(description='Import Consul & Vault keys from YAML file')
 parser.add_argument('--filename', action='append', help='YAML file to load')
@@ -15,14 +19,17 @@ parser.add_argument('--update', dest='update', action='store_true', help='Update
 args = parser.parse_args()
 
 c = consul.Consul()
+h = hvac.Client()
 
 def load_yaml(filename):
     stream = open(filename, 'r')
     data = yaml.load(stream, Loader=yaml.BaseLoader)
 
-    need_to_create_or_update = {}
+    consul_to_create_or_update = {}
+    vault_to_create_or_update = {}
 
-    for k, v in data['kv'].items():
+    print('Check Consul keys:')
+    for k, v in data.get(VAULT_KV_MOUNT, {}).items():
         current_raw_value = c.kv.get(k)
         if current_raw_value[1] is None:
             current_value = None
@@ -31,26 +38,51 @@ def load_yaml(filename):
 
         if current_value is None:
             print('{} => is missing'.format(k))
-            need_to_create_or_update[k] = v
+            consul_to_create_or_update[k] = v
         elif current_value != v:
             print('{}: {} != {}'.format(k, current_value, v))
-            need_to_create_or_update[k] = v
+            consul_to_create_or_update[k] = v
 
-    return need_to_create_or_update
+    print('Check Vault keys:')
+    for k, v in data.get('vault', {}).items():
+        try:
+            current_raw_value = h.secrets.kv.v1.read_secret(
+                mount_point=VAULT_KV_MOUNT,
+                path=k,
+            )
+            current_value = current_raw_value['data']
+        except(hvac.exceptions.InvalidPath):
+            current_value = None
 
-print('Check keys:')
+        if current_value is None:
+            print('{} => is missing'.format(k))
+            vault_to_create_or_update[k] = v
+        elif current_value != v:
+            print('{}: {} != {}'.format(k, current_value, v))
+            vault_to_create_or_update[k] = v
+
+    return (consul_to_create_or_update, vault_to_create_or_update)
+
 for filename in args.filename:
-    new_dict = load_yaml(filename)
-    need_to_create_or_update = {**need_to_create_or_update, **new_dict}
+    (new_consul, new_vault) = load_yaml(filename)
+    consul_to_create_or_update = {**consul_to_create_or_update, **new_consul}
+    vault_to_create_or_update = {**vault_to_create_or_update, **new_vault}
 
-if len(need_to_create_or_update) == 0:
+if len(consul_to_create_or_update) == 0 and vault_to_create_or_update == 0:
     print('Everything is OK. Exit now')
     sys.exit(0)
 
 if args.update:
     print('\nDo update:')
-    for k, v in need_to_create_or_update.items():
+    for k, v in consul_to_create_or_update.items():
         print('{}: {}'.format(k, v))
         c.kv.put(k, v)
+    for k, v in vault_to_create_or_update.items():
+        print('{}: {}'.format(k, v))
+        h.secrets.kv.v1.create_or_update_secret(
+            mount_point=VAULT_KV_MOUNT,
+            path=k,
+            secret=v,
+        )
 else:
     print('\nRun in dry mode, add "--update" parameter if you want update keys')
